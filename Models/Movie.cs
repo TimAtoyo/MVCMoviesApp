4042f5f8ce48e8a4c6e1928db3833d732fa09392
Timothy Atoyo <timothy.atoyo@gmail.com>
2024-06-12 18:10:48 +0100

    Add search by title functionality

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
@@ -6,9 +7,11 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+
     [DataType(DataType.Date)]
-    [Display(Name = "Release Date")]
+    // [Display(Name = "Release Date")]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
