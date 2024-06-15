9379b71a44dda9edf1edf4d348603d00ebd612e8
Timothy Atoyo <timothy.atoyo@gmail.com>
2024-06-15 19:34:54 +0100

    Final Commit hopefully

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a97fab1d78aab996a3d463471553d23d6b984c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+796e4ca9b1b51e7af8552977eac9594dce065975")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
